ee046d62076480ae8f7fe07419f2ca7dc3543739
Konrad Jachimstal <k.jachimstal@gmail.com>
2023-01-27 14:27:59 +0100

    Completed slider with responsive design

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import './sass/main.scss';
+import 'swiper/swiper-bundle.css';
+// eslint-disable-next-line
+import Swiper from 'swiper/swiper-bundle.esm.js';
+
+// eslint-disable-next-line
+window.swiper = new Swiper({
+  el: '.swiper',
+  speed: 300,
+  // cssMode: true,
+  // rewind: true,
+  slidesPerView: 1,
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+  navigation: {
+    prevEl: '.swiper-button-prev',
+    nextEl: '.swiper-button-next',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    bulletClass: 'comments__slider-element',
+    bulletActiveClass: 'comments__slider-element--active',
+  },
+});
