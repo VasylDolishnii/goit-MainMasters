11f043956cab57410bfa6470734dd7e634fb3919
Konrad Jachimstal <k.jachimstal@gmail.com>
2023-01-27 14:36:04 +0100

    Clear code

--- src/index.js
+++ src/index.js
@@ -1,14 +1,10 @@
 import './sass/main.scss';
 import 'swiper/swiper-bundle.css';
-// eslint-disable-next-line
 import Swiper from 'swiper/swiper-bundle.esm.js';
 
-// eslint-disable-next-line
 window.swiper = new Swiper({
   el: '.swiper',
   speed: 300,
-  // cssMode: true,
-  // rewind: true,
   slidesPerView: 1,
   scrollbar: {
     el: '.swiper-scrollbar',
